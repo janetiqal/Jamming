29631283672520ecc4e90e02caea6c5ea1cf36ca
Janet Iqal <j.iqal35@gmail.com>
2021-10-28 17:56:33 -0500

    3 components rendering and styled

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -1,19 +1,23 @@
-// import logo from './logo.svg';
-import { render } from 'react-dom';
+
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+
 import './App.css';
 
 function App() {
   return (
     <div>
-    <h1>Ja<span className="highlight">mmm</span>ing</h1>
-    <div className="App">
-      {/* <!-- Add a SearchBar component --> */}
-      <div className="App-playlist">
-        {/* <!-- Add a SearchResults component --> */}
-        {/* <!-- Add a Playlist component --> */}
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+        <SearchResults />
+        <Playlist />
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
