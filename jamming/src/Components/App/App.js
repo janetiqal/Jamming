24484efee8476778ea386449f1f1211a3e7c0f3a
Janet Iqal <j.iqal35@gmail.com>
2021-10-29 12:00:39 -0500

    added state to allow for searchbar value to be checked, added methods to app js to save search term and save playlist

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -26,6 +26,8 @@ class App extends React.Component {
     this.addTrack=this.addTrack.bind(this)
     this.removeTrack=this.removeTrack.bind(this)
     this.updatePlaylistName=this.updatePlaylistName.bind(this)
+    this.savePlaylist= this.savePlaylist.bind(this)
+    this.search= this.search.bind(this)
 
   }
   addTrack(track){
@@ -47,16 +49,23 @@ class App extends React.Component {
   updatePlaylistName(newName){
     this.setState({playlistName:newName})
   }
+
+  savePlaylist(){
+    const trackURIs= this.state.playlistTracks.map(track=> track.uri)
+  }
+  search(term){
+    console.log(term)
+  }
   
   render(){
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        <SearchBar />
+        <SearchBar  onSearch={this.search}/>
         <div className="App-playlist">
         <SearchResults searchResults={this.state.SearchResults} onAdd={this.addTrack}/>
-        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
+        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
         </div>
       </div>
     </div>
